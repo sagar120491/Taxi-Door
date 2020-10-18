b3a9c7ac907728a8a5954ff41b21bdfaa159061d
sagar.shelke <cag.sagar121@gmail.com>
2020-10-18 20:56:58 +0530

    git commit layer2

---
+++ TaxiDoor.Service/VendorService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using TaxiDoor.Data;
+using TaxiDoor.Persistance;
+
+namespace TaxiDoor.Service
+{
+    public class VendorService
+    {
+
+        public List<Vendor> GetAllVendors()
+        {
+
+            TaxiContext context = new TaxiContext();
+            return context.Vendors.ToList();
+
+
+            return null;
+
+        }
+
+
+        public Vendor GetById(int id)
+        {
+            TaxiContext context = new TaxiContext();
+            return context.Vendors.FirstOrDefault(x => x.Id == id);
+        }
+
+
+
+
+
+        public Vendor GetByEmail(int id) 
+        {
+            TaxiContext context = new TaxiContext();
+            return context.Vendors.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
